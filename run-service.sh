3058ded0f59fe29cb16c3df2e981efd30a1e44a2
Jean-Marc Tremeaux <jm.tremeaux@gmail.com>
2016-03-01 11:29:06 +0100

    Make Docker use a volume instead of a volume container

--- run-service.sh
+++ run-service.sh
@@ -2,6 +2,6 @@
 docker rm -f sismics_home
 docker run \
     -d --name=sismics_home --restart=always \
-    --volumes-from=sismics_home_data \
+    -v sismics_home_data:/data \
     -e 'VIRTUAL_HOST_SECURE=myhome.bgamard.org' -e 'VIRTUAL_PORT=80' \
     sismics/home:latest
